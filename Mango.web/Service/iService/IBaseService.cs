e0986ee9d7f70ee8d09c361436f0aa4da7c2cb0f
tofuaccel <pingu28950539@gmail.com>
2024-10-31 20:38:10 +0800

    25. Base Service Interface

---
+++ Mango.web/Service/iService/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.CouponAPI.Models.Dto;
+using Mango.web.Models;
+
+namespace Mango.web.Service.iService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(RequestDto requestDto);
+    }
+}
